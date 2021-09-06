e9e1aece0d5b6257ad0d58eb78973a1a92048c75
Sam Shock <sam.shock924@gmail.com>
2021-09-06 16:08:12 -0400

    dang buttons >_<

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function addItem() {
     let list = document.getElementsByClassName("toDoList");
     let listItem = document.createElement("toDoItem");
-    
+    let listInput = document.getElementById("toDoInput");
+    listItem.appendChild(document.createTextNode(listItem.value));
+    // console.log("hello");
 }
