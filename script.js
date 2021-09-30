ddd0c4955e914856a25261947f7b9a90d986e129
Sam Shock <sam.shock924@gmail.com>
2021-09-29 20:13:31 -0400

    added complete option

--- script.js
+++ script.js
@@ -8,22 +8,18 @@ function addItem() {
     const listItem = document.createElement("li");
     listItem.setAttribute('id', 'toDoItem');
     listItem.appendChild(addListItem);
-    // const completeButton = document.createElement("button");
-    // completeButton.setAttribute('class', 'completeButton');
-    // completeButton.textContent =     listItem.appendChild(completeButton);
     const deleteButton = document.createElement("button");
     deleteButton.setAttribute('class', 'deleteButton');
     deleteButton.textContent = 'X';
     listItem.appendChild(deleteButton);
     list.appendChild(listItem);
-    deleteButton.addEventListener('click', deleteItem)
+    deleteButton.addEventListener('click', () => deleteItem(listItem))
+    listItem.addEventListener('click', function(){
+        listItem.style.textDecoration = "line-through";
+    })
 }
 
-
-function deleteItem() {
-    let toDoItem = document.getElementById("toDoItem");
-    toDoItem.remove();
-
+function deleteItem(listItem) {
+    listItem.remove();
 }
-
 listButton.addEventListener('click', addItem);
